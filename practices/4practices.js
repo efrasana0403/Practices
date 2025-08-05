6d5055749ba02f1cf83c51763eb7f1b94535e3d1
Armando Sanabria <ash12.10.1999@gmail.com>
2025-08-04 20:56:57 -0500

    first attempt, failed

---
+++ practices/4practices.js
@@ -0,0 +1,11 @@
+/**crear un mini sistema que nos permita registar los alumnos que estan presente (p) u ausentes (a) en clase.
+ * pasado los tres dias, mostras la situacion final de todos los alumnos (number total de presentes y ausentes)
+ * se puede tener un maximo de 10% de ausencias por semestre, si es mayor, aclarar que estam reprobados
+ */
+
+const student = [0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1,]
+// 1= p && 0= a
+
+
+
+console.log(student.length);
