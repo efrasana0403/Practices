03af4dfe57f455f10036c82c58964f9ded028551
Armando Sanabria <ash12.10.1999@gmail.com>
2025-08-06 20:59:43 -0500

    still cannot condition divison logic

--- practices/5practices.js
+++ practices/5practices.js
@@ -1,8 +1,15 @@
-// creas una calculadora que haga diferentes operaciones
-
-
-function calculator(a, b) {
-    return a + b;
+function calculate(a, b, operationFn) {
+    return operationFn(a, b);
 }
 
-console.log(calculator(3, 3));
+
+// Define operations
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const divide = (a, b) => a / b;
+const multiply = (a, b) => a * b;
+// Usage
+console.log(calculate(10, 2, add));        // 12
+console.log(calculate(10, 2, subtract));   // 8
+console.log(calculate(10, 0, divide));   // infinite
+console.log(calculate(10, 2, multiply));   // 20
