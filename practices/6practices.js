4c554161bbe77c1da7179083cf8e816a038dd3ac
Armando Sanabria <ash12.10.1999@gmail.com>
2025-08-09 21:51:28 -0500

    instructions

---
+++ practices/6practices.js
@@ -0,0 +1,21 @@
+/**creas un sistema para mostrar particularidades de 3 celulares...
+ * color
+ * peso
+ * resolucion de camara
+ * resolucion de pantalla
+ * y memoria RAM
+ * Tambien deben poder prender, reiniciar, tomar fotos, y grabar
+ */
+
+/**celulares de alta gama pueden hacer lo mismo que los anteriores y mas, aparte pueden gramar en camara super lenta
+ * reconocimiento facial y una camara extra
+ * debemos implementar todas esas cualidades pero ahora en los celulares de alta gama.
+ */
+
+/**cofla ahora debe saber cuales dos aplicacines de 7 debe descargar
+ * apps deben tener:
+ * cantidad de descarga,
+ * puntuacion y 
+ * peso
+ * se deben poder instalar, abrir, cerrar, y desintalar.
+ */
