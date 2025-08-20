7f9d36df6ae7a5aa0b2ab86f589ef32688af626f
Armando Sanabria <ash12.10.1999@gmail.com>
2025-08-20 13:28:47 -0500

    fizz buzz exercise. Used two if within the same function, and used [] to access specifically each element of the array also used for loop to loop among every element of the array.

---
+++ beyond/01beyond.js
@@ -0,0 +1,29 @@
+/**
+ 1- FizzBuzz
+Description: Write a function that prints numbers from 1 to 20.
+If the number is divisible by 3, print "Fizz".
+If divisible by 5, print "Buzz".
+If divisible by both, print "FizzBuzz".
+// Output: 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, ...
+ */
+
+const numbers = Array.from({ length: 20 }, (_, i) => i + 1);
+
+const FizzBuzz = (arr) => {
+    for (let index = 0; index < arr.length; index++) {
+        if (arr[index] % 3 === 0) {
+            arr[index] = "fizz";
+        } if (arr[index] % 5 === 0) {
+            arr[index] = "buzz";
+        }
+    } return arr
+
+}
+
+console.log(FizzBuzz(numbers)); /** outputs: [
+  1,      2,      'fizz', 4,
+  'buzz', 'fizz', 7,      8,
+  'fizz', 'buzz', 11,     'fizz',
+  13,     14,     'fizz', 16,
+  17,     'fizz', 19,     'buzz'
+] */
