06ab36e1b20d34afcfa8c681629ef022d1f65cae
Armando Sanabria <ash12.10.1999@gmail.com>
2025-08-20 13:45:52 -0500

    fizz buzz exercise. Used three if() within the same function, and used arr[] to access specifically each element of the array  also used for loop to loop among every element of the array.

--- beyond/01beyond.js
+++ beyond/01beyond.js
@@ -11,11 +11,14 @@ const numbers = Array.from({ length: 20 }, (_, i) => i + 1);
 
 const FizzBuzz = (arr) => {
     for (let index = 0; index < arr.length; index++) {
-        if (arr[index] % 3 === 0) {
+        if (arr[index] % 3 === 0 && arr[index] % 5 === 0) {
+            arr[index] = "FizzBuzz";
+        } if (arr[index] % 3 === 0) {
             arr[index] = "fizz";
         } if (arr[index] % 5 === 0) {
             arr[index] = "buzz";
         }
+
     } return arr
 
 }
@@ -24,6 +27,6 @@ console.log(FizzBuzz(numbers)); /** outputs: [
   1,      2,      'fizz', 4,
   'buzz', 'fizz', 7,      8,
   'fizz', 'buzz', 11,     'fizz',
-  13,     14,     'fizz', 16,
+  13,     14,     'fizzBuzz', 16,
   17,     'fizz', 19,     'buzz'
 ] */
