ab9c2e6f503884273f7c789a7fac6acda5fb6f34
Armando Sanabria <ash12.10.1999@gmail.com>
2025-08-25 18:58:23 -0500

    oop phone exercises, learned parent and son classes, learned how to create a object and how to inherit properties from the parent to the son class

---
+++ beyond/05beyond.js
@@ -0,0 +1,71 @@
+class Cellphone {
+    constructor(color, weight, cmraRes, scrnRes, ram,) {
+        this.color = color;
+        this.weight = weight;
+        this.cameraResolution = cmraRes;
+        this.screenResolution = scrnRes;
+        this.ramMemory = ram;
+        this.power = false;
+    }
+    information() {
+        return `Phone information:
+- Weight: ${this.weight}
+- Color: ${this.color}
+- Camera Resolution: ${this.cameraResolution}
+- Screen Resolution: ${this.screenResolution}
+- RAM: ${this.ramMemory}
+- Power: ${this.power ? "On" : "Off"}
+        `.trim();
+    }
+    isPoweredOn() {
+        if (this.power === false) {
+            return "phone is turned off";
+        } else {
+            return "phone is turned on";
+        }
+    }
+    powerOnOff() {
+        if (this.power === false) {
+            this.power = true;
+            return "phone is turned on";
+        } else {
+            this.power = false;
+            return "phone is turned off";
+        }
+    }
+    restart() {
+        if (this.power === true) {
+            return "phone is restarting";
+        } else {
+            this.power = true;
+            return "phone is turning on";
+        }
+    }
+}
+
+const iPhone25 = new Cellphone("red", "150g", "12 megapixels", "high resolution", "4GB")
+
+// console.log(iPhone25.powerOnOff()); // Phone is turned on
+// console.log(iPhone25.powerOnOff()); // Phone is turned off
+// console.log(iPhone25.isPoweredOn()); // Phone is turned off
+// console.log(iPhone25.restart());     // Phone is turning on
+// console.log(iPhone25.isPoweredOn()); // Phone is turned on
+// console.log(iPhone25.information()); //outputs phone info 
+
+class HighGammaCellphone extends Cellphone {
+    constructor(color, weight, cmraRes, scrnRes, ram, xtraCmra) {
+        super(color, weight, cmraRes, scrnRes, ram);
+        this.xtraCameraRes = xtraCmra;
+    }
+    HGInformation() {
+        return `
+        ${this.information()}
+-Extra camara resolution: ${this.xtraCameraRes}
+        `.trim()
+    }
+}
+const iPhone25Pro = new HighGammaCellphone("black", "100g", "14 Megapixels", "4K resolution", "8GB", "20 Megapixels");
+// console.log(iPhone25Pro.HGInformation()); // outputs phone info 
+
+/**I can can call a function from the parent class on an object in the inherited class*/
+
