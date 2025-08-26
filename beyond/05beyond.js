5730f7f2efd30efc7bb7eb634863160364943265
Armando Sanabria <ash12.10.1999@gmail.com>
2025-08-25 20:33:27 -0500

    oop phone exercises, learned parent and son classes, learned how to create a object and how to inherit properties from the parent to the son class

--- beyond/05beyond.js
+++ beyond/05beyond.js
@@ -45,7 +45,7 @@ class Cellphone {
 
 const iPhone25 = new Cellphone("red", "150g", "12 megapixels", "high resolution", "4GB")
 
-console.log(iPhone25.powerOnOff()); // Phone is turned on
+// console.log(iPhone25.powerOnOff()); // Phone is turned on
 // console.log(iPhone25.powerOnOff()); // Phone is turned off
 // console.log(iPhone25.isPoweredOn()); // Phone is turned off
 // console.log(iPhone25.restart());     // Phone is turning on
@@ -72,5 +72,5 @@ const iPhone25Pro = new HighGammaCellphone("black", "100g", "14 Megapixels", "4K
  * classes starts with caps by convention
  * constroctor(), super(), and methods() are created within the class
  * used `class "inheritedClass" extends "parent class"{}` to connect son and parents classes
- * used super () to inherit properties from the parento onto the son class.
+ * used super () to inherit properties from the parento onto the son class
 */
